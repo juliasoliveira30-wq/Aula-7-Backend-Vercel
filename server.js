ed38385c437c526ebbcf70ebf0c5e9e767a4c42c
Júlia Santos <julia.s.oliveira30@aluno.senai.br>
2026-05-08 08:53:36 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ const logger = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 app.use(logger);
@@ -53,9 +54,11 @@ app.get('/', (req, res) => {
 
 const rotasCategorias = require('./routes/categorias');
 const rotasProdutos = require('./routes/produtos');
+const rotasPedidos = require('./routes/pedidos'); 
 
 app.use('/api/categorias', rotasCategorias);
 app.use('/api/produtos', rotasProdutos);
+app.use('/api/pedidos', rotasPedidos);
 
 app.use((req, res, next) => {
     res.status(404).json({
@@ -83,6 +86,8 @@ app.listen(PORTA, () => {
     console.log(`   POST   /api/produtos`);
     console.log(`   PUT    /api/produtos/:id`);
     console.log(`   DELETE /api/produtos/:id`);
+    console.log(`   GET    /api/pedidos`);
+    console.log(`   POST   /api/pedidos`);
     console.log('');
     console.log('💣 Rota de teste de erro:');
     console.log(`   GET   /api/produtos/erro-teste`);
